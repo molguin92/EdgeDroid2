26c8caaf1b77f695ece7c3e3bc4ca1d0e4aec8a1
Manuel Olguín Muñoz <manuel@olguinmunoz.xyz>
2024-05-17 00:14:19 -0400

    exps

--- analysis_2023/experiments_2024/inner/run_experiment.sh
+++ analysis_2023/experiments_2024/inner/run_experiment.sh
@@ -3,7 +3,9 @@ set -x
 
 OUT_DIR="/opt/output"
 
-tc qdisc add dev lo root netem delay "${LATENCY}"ms "${VARIANCE}"ms distribution "${DIST:-"pareto"}"
+# more latencies
+# autocorrelation
+tc qdisc add dev lo root netem delay "${LATENCY}ms" "${VARIANCE}ms" "${CORREL}%" distribution "${DIST:-"pareto"}"
 
 python3 /opt/edgedroid-experiments/experiment_server.py --truncate "${NUM_STEPS}" -v -o "${OUT_DIR}" localhost 5000 "${TRACE}" &
 python3 /opt/edgedroid-experiments/experiment_client.py --truncate "${NUM_STEPS}" -v -o "${OUT_DIR}" "${MODEL}" localhost 5000 "${TRACE}"
--- analysis_2023/experiments_2024/run_experiments.sh
+++ analysis_2023/experiments_2024/run_experiments.sh
@@ -9,16 +9,18 @@ VARIANCE=10  # also one-way
 NUM_STEPS=50
 REPS_PER_EXP=10
 
-OUT_DIR="./output/lat${LATENCY}_var${VARIANCE}"
-mkdir -p "${OUT_DIR}"
+for CORREL in 0 25 50 75 100; do
+  OUT_DIR="./output/lat${LATENCY}_var${VARIANCE}_corr${CORREL}"
+  mkdir -p "${OUT_DIR}"
 
-for REP in $(seq 1 ${REPS_PER_EXP}); do
-  for MODEL in legacy first-order first-order-median curve-high-neuro curve-low-neuro; do
-    EXP_OUT_DIR="${OUT_DIR}/${MODEL}_${REP}_of_${REPS_PER_EXP}"
-    mkdir -p "${EXP_OUT_DIR}"
-    docker run --rm -it --cap-add NET_ADMIN -v "${EXP_OUT_DIR}:/opt/output"\
-      -e LATENCY=${LATENCY} -e VARIANCE=${VARIANCE} -e \
-      NUM_STEPS=${NUM_STEPS} -e TRACE=square00 -e MODEL=${MODEL} "experiment_client"
+  for REP in $(seq 1 ${REPS_PER_EXP}); do
+    for MODEL in legacy first-order first-order-median curve-high-neuro curve-low-neuro; do
+      EXP_OUT_DIR="${OUT_DIR}/${MODEL}_${REP}_of_${REPS_PER_EXP}"
+      mkdir -p "${EXP_OUT_DIR}"
+      docker run --rm -it --cap-add NET_ADMIN -v "${EXP_OUT_DIR}:/opt/output"\
+        -e LATENCY=${LATENCY} -e VARIANCE=${VARIANCE} -e CORREL=${CORREL} \
+        -e NUM_STEPS=${NUM_STEPS} -e TRACE=square00 -e MODEL=${MODEL} "experiment_client"
+    done
   done
 done
 
