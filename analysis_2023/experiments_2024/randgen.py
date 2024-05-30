e84244201d9f2469f8235910b34b6ec5fc5a46b6
Manuel Olguín Muñoz <manuel@olguinmunoz.xyz>
2024-05-30 09:46:41 -0400

    data

--- analysis_2023/experiments_2024/randgen.py
+++ analysis_2023/experiments_2024/randgen.py
@@ -8,6 +8,8 @@ __all__ = ["gen_autocorrel"]
 def gen_autocorrel(
     dist: sp.stats.rv_continuous, loc: float, rho: float, **kwargs
 ) -> Iterator[float]:
+
+    loc = loc * (1 / rho)
     while True:
         value = dist.rvs(loc=(loc * rho), size=1, **kwargs).take(0)
         yield value
